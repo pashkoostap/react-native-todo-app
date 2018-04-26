c307bbcbf039dc0f464dc1c12569c93fa9358bce
pashkoostap <pashko.ostap@gmail.com>
2018-04-26 14:42:27 +0300

    updated localization configs

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,10 +1,12 @@
 import I18n from "react-native-i18n";
 import en from "./en";
+import uk from "./uk";
 
 I18n.fallbacks = true;
 
 I18n.translations = {
-  en
+  en,
+  uk
 };
 
 export default I18n;
---
+++ src/localization/uk.js
@@ -0,0 +1,6 @@
+export default {
+  home: {
+    buttonText: "Додати нове завдання",
+    noTodosText: "Немає завдань"
+  }
+};
